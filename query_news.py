ee43ff49becf308d4f6a6ad7762c4792844bdb6e
Dominic <lovric.domagoj1@gmail.com>
2023-06-18 18:00:18 +0100

    Querying our db

--- query_news.py
+++ query_news.py
@@ -1,11 +1,17 @@
-import os
+import os, config
 from dotenv import load_dotenv
 from llama_index import StorageContext, load_index_from_storage
 import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY'] = config.OPENAI_API_KEY
 
 # Load the data
-storage_context = StorageContext.from_defaults(persist_dir="index_news.json")
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
 index = load_index_from_storage(storage_context)
 
+# Set the query engine
+query_engine = index.as_query_engine()
+response = query_engine.query("What are some near-term risks to Nvidia?")
+
+print(response)
