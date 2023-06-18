653a39ff130a83ab09304409e5bde09e7a7ab5f9
Dominic <lovric.domagoj1@gmail.com>
2023-06-18 18:58:02 +0100

    Query successful

--- query_news.py
+++ query_news.py
@@ -1,10 +1,11 @@
 import os, config
 from dotenv import load_dotenv
 from llama_index import StorageContext, load_index_from_storage
-import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
 os.environ['OPENAI_API_KEY'] = config.OPENAI_API_KEY
+import openai
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 
 # Load the data
 storage_context = StorageContext.from_defaults(persist_dir="./storage")
@@ -12,6 +13,9 @@ index = load_index_from_storage(storage_context)
 
 # Set the query engine
 query_engine = index.as_query_engine()
-response = query_engine.query("What are some near-term risks to Nvidia?")
+# response = query_engine.query("What are some near-term risks to Nvidia?")
+response = query_engine.query("What is META doing in regards to the AI?")
+
+
 
 print(response)
